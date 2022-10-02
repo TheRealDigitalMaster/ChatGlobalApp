8fc404a869e90ceb05253df3de2097ffb9951aeb
-DigitalMaster- <112659665+TheRealDigitalMaster@users.noreply.github.com>
2022-10-02 08:13:11 -0400

    Update Chat.js

--- Discord/Chat.js
+++ Discord/Chat.js
@@ -1,6 +1,6 @@
 // PS! Replace this with your own channel ID
 // If you use this channel ID your app will stop working in the future
-const CLIENT_ID = 'qGgv0UdiYFU60lup';
+const CLIENT_ID = 'bRD0sxvgFRsHRVHz';
 
 const drone = new ScaleDrone(CLIENT_ID, {
   data: { // Will be sent out as clientData via events
@@ -59,8 +59,8 @@ drone.on('error', error => {
 });
 
 function getRandomName() {
-  const adjs = [ "-Autumn", "-Hidden", "-Bitter", "-Misty", "-Silent", "-Empty", "-Dry", "-Dark", "-Summer", "-Icy", "-Stupid", "-Quiet", "-White", "-Cool", "-Spring", "-Winter", "-Patient", "-Blood", "-Dawn", "-Crimson", "-Wispy", "-Weathered", "-Blue", "-Billowing", "-Broken", "-Cold", "-Damp", "-Falling", "-Frosty", "-Green", "-Meme", "-Late", "-Lingering", "-Bold", "-Little", "-Morning", "-Muddy", "-Old", "-Red", "-Rough", "-Still", "-Small", "-Sparkling", "-Throbbing", "-Shy", "-Wandering", "-Withered", "-Wild", "-Black", "-Young", "-Waterfall", "-Solitary", "-Fragrant", "-Aged", "-Snowy", "-Piano", "-Sun", "-Void", "-Divine", "-Polished", "-Ancient", "-Purple", "-Lively", "-Nameless"];
-  const nouns = ["Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-", "Master-"];
+  const adjs = [ "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy", "-DiscordBuddy"];
+  const nouns = ["1-", "2-", "3-", "4-", "5-", "6-", "7-", "8-", "9-", "10-", "11-", "12-", "13-", "14-", "15-", "16-", "17-", "18-", "19-", "20-", "21-", "22-", "23-", "24-", "25-", "26-", "27-", "28-", "29-", "30-", "31-", "32-", "33-", "34-", "35-", "36-", "37-", "38-", "39-", "40-", "41-", "42-", "43-", "44-", "45-", "46-", "47-", "48-", "49-", "50-", "51-", "52-", "53-", "54-", "55-", "56-", "57-", "58-", "59-", "60-", "61-", "62-", "63-", "64-"];
   return (
     adjs[Math.floor(Math.random() * adjs.length)] +
     "_" +
@@ -69,8 +69,9 @@ function getRandomName() {
 }
 
 function getRandomColor() {
-  return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+  return '#' + Math.floor(Math.random() * 0xff0000).toString(16);
 }
+
 
 //------------- DOM STUFF
 
